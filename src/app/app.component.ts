293689551a877539e7ab5f18fa5b4df8b03f9930
Esra-Sahinler <esrasahinler4@gmail.com>
2021-03-29 22:06:48 +0300

    17.gün ödevi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recapdatabase';
+  user:string='Esra Şahinler';
 }
---
+++ src/app/models/car.ts
@@ -0,0 +1,8 @@
+export interface Car{
+    Id:number;
+    brandName:string;
+    colorName:string;
+    modelYear:Date;
+    dailyPrice:number;
+    description:string;
+}
